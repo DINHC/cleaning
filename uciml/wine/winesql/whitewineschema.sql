292ca07e08219c4d273487cefb6ed5774d97e671
Dinh Chau <dinhchau001@gmail.com>
2023-09-29 14:08:45 -0700

    Exported the cleaned white wine csv. Also preparing to add to pgadmin

---
+++ uciml/wine/winesql/whitewineschema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE whitewine (
+	fixedacidity INT, 
+	volatileacidity INT,
+	citricacidity INT, 
+	residualsugar INT,
+	chlorides INT,
+	freesulfurdioxide INT,
+	totalsulfurdioxide INT,
+	density INT, 
+	pH INT, 
+	sulphates INT, 
+	alchohol INT,
+	quality INT
+)
